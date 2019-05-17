b6997dfca13956924f9909b05a98a75182159707
codyrush <codyrush@gmail.com>
2019-05-16 17:55:47 -0700

    intial push

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,31 @@ import React, { Component } from "react";
 import CoinUsd from "./CoinUsd";
 import "./App.css";
 
+const coinList = [
+  "bitcoin",
+  "ripple",
+  "ethereum",
+  "neo",
+  "ong",
+  "ont"
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CoinUsd coinId={'bitcoin'} />
-        <CoinUsd coinId={'ripple'} />
-        <CoinUsd coinId={'ethereum'} />
+      
+        <div className="row">
+          <div className="col">&nbsp;</div>
+          <div className="col text-right font-weight-bold">USD</div>
+          <div className="col text-right font-weight-bold">Qty</div>
+          <div className="col text-right font-weight-bold">Subtotal</div>
+        </div>
+        {coinList.map(coin => <CoinUsd key={coin} coinId={coin} quantity={42} />)}
+        <CoinUsd coinId={'bitcoin'} quantity={42} />
+        <CoinUsd coinId={'ripple'} quantity={42} />
+        <CoinUsd coinId={'ethereum'} quantity={42} />
+        {console.log(coinList)}
       </div>
     );
   }
--- client/src/CoinUsd/index.js
+++ client/src/CoinUsd/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NumberFormat from 'react-number-format';
 
 // import Container from 'react-bootstrap/Container';
 // import Row from 'react-bootstrap/Row';
@@ -32,7 +33,9 @@ class CoinUsd extends Component {
             <React.Fragment>
               <div className="row">
                 <div className="col">{this.state.name}</div>
-                <div className="col">{this.state.usd}</div>
+                <div className="col text-right">{this.state.usd}</div>
+                <div className="col text-right">{this.props.quantity}</div>
+                <div className="col text-right"><NumberFormat value={this.state.usd * this.props.quantity} displayType={'text'} decimalScale={2} thousandSeparator={true} renderText={value => <div>{value}</div>} /></div>{/* prefix={'$'} */}
               </div>
             </React.Fragment>
          );
